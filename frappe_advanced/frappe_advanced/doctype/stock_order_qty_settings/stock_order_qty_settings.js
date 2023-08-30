bc5394d8617517ecffb745679d84d6f2d96c0945
Madcheese101 <40042269+Madcheese101@users.noreply.github.com>
2023-08-30 20:54:29 +0200

    Feat: Stock Settings
    Add: stock settings doctype which sets the max quantity of a product that could be available in the store warehouse for certain sizes.
    this feature will be used in the feature to make material transfer requests and reports.

---
+++ frappe_advanced/frappe_advanced/doctype/stock_order_qty_settings/stock_order_qty_settings.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2023, MadCheese and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Stock Order QTY Settings', {
+	refresh: function(frm) {
+		frm.call('set_table')
+	},
+	onload: function(frm){
+		frm.call('set_table')
+	}
+});
---
+++ frappe_advanced/frappe_advanced/doctype/stock_order_qty_settings/stock_order_qty_settings.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2023, MadCheese and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+import frappe
+
+class StockOrderQTYSettings(Document):
+	@frappe.whitelist()
+	def set_table(self):
+		filters = {'parent':"المقاس"}
+		existing_size = None
+		if self.max_qty_per_size:
+			existing_size = frappe.db.get_list('Stock Max QTY per Size',
+				 fields=["size"],
+				 filters={"parent": self.name},
+				 order_by='size',
+				 pluck="size")
+			filters['attribute_value'] = ['not in',existing_size]
+
+		size_list = frappe.db.get_list('Item Attribute Value',
+				 fields=["attribute_value as size", "'0' as max_qty"],
+				 filters=filters,
+				 order_by='attribute_value')		
+		
+		if size_list:
+			for size in size_list:
+				self.append("max_qty_per_size", size)
+		
+		if size_list and existing_size:
+			frappe.msgprint("تم إضافة مقاسات جديدة الى نهاية القائمة")
