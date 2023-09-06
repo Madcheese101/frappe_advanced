fc6e32aa8065431cc4657fc8cf81d20ab6bcbaa1
Madcheese101 <40042269+Madcheese101@users.noreply.github.com>
2023-09-06 03:23:13 +0200

    Add: Auto split batch for item group in advanced settings

--- frappe_advanced/frappe_advanced/doctype/advanced_settings/advanced_settings.js
+++ frappe_advanced/frappe_advanced/doctype/advanced_settings/advanced_settings.js
@@ -16,11 +16,24 @@ frappe.ui.form.on('Advanced Settings', {
 	},
 	write_off_limit_warning: function(frm) {
 		frm.events.show_hide_fields(frm);
+	},	
+	auto_split_batch: function(frm) {
+		frm.events.show_hide_fields(frm);
 	},
 	show_hide_fields: function(frm){
 		frm.toggle_display("account_transfer_days_limit", (frm.doc.account_transfer_limit_warning));
 		frm.toggle_display("stock_entry_days_limit", (frm.doc.stock_entry_not_accepted_warning));
 		frm.toggle_display("exclude_account_numbers", (frm.doc.account_transfer_limit_warning));
 		frm.toggle_display("write_off_limit", (frm.doc.write_off_limit_warning));
+		frm.toggle_display("for_item_groups", (frm.doc.auto_split_batch));
+	},
+	setup:function(frm){
+		frm.set_query('for_item_groups', () => {
+			return {
+				filters: {
+					is_group: 0
+				}
+			}
+		})
 	}
 });
