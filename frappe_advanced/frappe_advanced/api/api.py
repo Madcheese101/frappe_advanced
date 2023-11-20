78bcbec00920e32d0bfe61546ec6f89090a0e888
Madcheese101 <40042269+Madcheese101@users.noreply.github.com>
2023-11-20 17:01:27 +0200

    fix: auto_close_shift() btn validation

--- frappe_advanced/frappe_advanced/api/api.py
+++ frappe_advanced/frappe_advanced/api/api.py
@@ -27,36 +27,35 @@ def set_title(title, doc_name):
 
 @frappe.whitelist()
 def auto_close_shift():
-    if frappe.db.get_single_value('Advanced Settings', 'auto_close_shift'):
-        posawesome_exists = "posawesome" in frappe.get_installed_apps()
-        if posawesome_exists:
-            posProfile =  frappe.db.get_list('POS Profile',
+    posawesome_exists = "posawesome" in frappe.get_installed_apps()
+    if posawesome_exists:
+        posProfile =  frappe.db.get_list('POS Profile',
+                            pluck="name")
+        if posProfile and len(posProfile) == 1:
+            from posawesome.posawesome.doctype.pos_closing_shift.pos_closing_shift import (
+                make_closing_shift_from_opening,
+                submit_closing_shift
+                )
+            
+            filters = {'status': 'Open', 
+                    'docstatus': 1,
+                    'pos_profile': posProfile[0],
+                    'pos_closing_shift': ['in', ['', None]]}
+            opening_shifts =  frappe.db.get_all('POS Opening Shift',
+                                filters=filters,
                                 pluck="name")
-            if posProfile and len(posProfile) == 1:
-                from posawesome.posawesome.doctype.pos_closing_shift.pos_closing_shift import (
-                    make_closing_shift_from_opening,
-                    submit_closing_shift
-                    )
-                
-                filters = {'status': 'Open', 
-                        'docstatus': 1,
-                        'pos_profile': posProfile[0],
-                        'pos_closing_shift': ['in', ['', None]]}
-                opening_shifts =  frappe.db.get_all('POS Opening Shift',
-                                    filters=filters,
-                                    pluck="name")
-                
-                if len(opening_shifts) > 0:
-                    for open_shift in opening_shifts:
-                        open_doc = vars(frappe.get_doc("POS Opening Shift", open_shift))
-                        closing_doc = make_closing_shift_from_opening(open_doc)
+            
+            if len(opening_shifts) > 0:
+                for open_shift in opening_shifts:
+                    open_doc = vars(frappe.get_doc("POS Opening Shift", open_shift))
+                    closing_doc = make_closing_shift_from_opening(open_doc)
+                    
+                    for payment in closing_doc.payment_reconciliation:
+                        payment.closing_amount = payment.expected_amount
+                        payment.difference = 0
                         
-                        for payment in closing_doc.payment_reconciliation:
-                            payment.closing_amount = payment.expected_amount
-                            payment.difference = 0
-                            
-                        submit_closing_shift(vars(closing_doc))
-                else:
-                    frappe.msgprint('لا يوجد مناوبات لإغلاقها')
+                    submit_closing_shift(vars(closing_doc))
             else:
-                frappe.msgprint('المستخدم ليس موظف مبيعات')
+                frappe.msgprint('لا يوجد مناوبات لإغلاقها')
+        else:
+            frappe.msgprint('المستخدم ليس موظف مبيعات')
