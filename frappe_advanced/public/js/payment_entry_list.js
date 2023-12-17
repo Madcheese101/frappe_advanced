910ae1e07f79584eb0f756b2b2579796f254dbfb
Madcheese101 <40042269+Madcheese101@users.noreply.github.com>
2023-12-17 11:07:05 +0200

    fix: confirm dialog for auto close shift button

--- frappe_advanced/public/js/payment_entry_list.js
+++ frappe_advanced/public/js/payment_entry_list.js
@@ -1,17 +1,19 @@
-function close_pos_shift(listview) {
-	// console.log("ButtonFunction");
-	// frappe.msgprint("ButtonFunction");
-    frappe.call({
-        method: 'frappe_advanced.frappe_advanced.api.api.auto_close_shift',
-    });
-}
-
 frappe.listview_settings['Payment Entry'] = {
 
 	refresh: function(listview) {
         if (!frappe.user_roles.includes("Accounts Manager")){
             listview.page.add_inner_button(__('إغلاق مناوبة الموظفين'), function() {
-                close_pos_shift(listview);
+                let line_one = 'هذه العملية يجب ان تتم قبل اغلاق اليوم فقط'
+                let line_two = 'هل توافق على هذه العملية؟'
+                frappe.confirm(line_one+'<br>'+line_two,
+                () => {
+                    // action to perform if Yes is selected                   
+                    frappe.call({
+                        method: 'frappe_advanced.frappe_advanced.api.api.auto_close_shift',
+                    });
+                }, () => {
+                    // action to perform if No is selected
+                })
             });
         }
         listview.page.add_inner_button(__('رصيد الخزائن حاليا'), function() {
